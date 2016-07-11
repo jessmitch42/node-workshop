418c22997c93f62022150c14d5ef71ab7ef0fd20
JessMitch <jessicamitch42@gmail.com>
2016-07-11 20:47:52 +0000

    hello world

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,26 @@
+// Exercise 1
+
 function firstProgram() {
     console.log("Hello World");
     setTimeout(function() {console.log("Hellow World Again");}, 10000)
     
 }
 
-firstProgram();
+// firstProgram();
+
+// A wild interval has appeared! Exercise 2
+
+function everyTenSec () {
+    setTimeout(function () {
+        console.log("Hello World!");
+        setTimeout(function() {
+            console.log("Hello World!");
+            everyTenSec(); 
+        }, 10000);
+    }, 10000);
+}
+
+// everyTenSec
+
+// Getting some data: Exercise 3
+
