8f36f989a134113f8db17917b95b7ce4ec43a8b2
JessMitch <jessicamitch42@gmail.com>
2016-07-11 20:45:02 +0000

    Number Guessing Gamegit add number-guessing-game.js

---
+++ number-guessing-game.js
@@ -0,0 +1,44 @@
+/* Number guessing game!
+
+Create a file called number-guessing-game.js.
+
+In this file, re-write your number guessing game (from the basic javascript 
+workshop) for the command line!
+
+Instead of using prompt and alert, you will have to use capabilities from 
+NodeJS and any external module. HINT: there is an npm library called prompt 
+that can help you with that :)
+*/
+
+var prompt = require("prompt");
+
+
+
+function gameTime() {
+    var numToGuess = Math.floor(Math.random() * 100);
+    console.log(numToGuess);
+    
+    function guessingGame() {
+        return prompt.get("guess", function(err, result) {
+            
+            if (err) {
+                console.log("Oops!!! There was an error. :(");
+            }
+            else {
+                if (Number(result.guess) === numToGuess) {
+                    console.log("You got it!! Woo!");
+                }
+                else if (Number(result.guess) > numToGuess) {
+                    console.log("Too high! Try again!");
+                    guessingGame();
+                }
+                else if (Number(result.guess) < numToGuess) {
+                    console.log("Too low! Try again!");
+                    guessingGame();
+                }
+            }
+        });
+    }
+    guessingGame();
+}
+gameTime();
